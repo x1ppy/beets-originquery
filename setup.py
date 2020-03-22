fb8d830b4ec4f6b51a1a7b31fc23cae0d5e71395
x1ppy <>
2020-03-21 23:51:04 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="beets-originquery",
+    version="1.0.0",
+    author="x1ppy",
+    packages=['beetsplug'],
+    author_email="",
+    description="Integrates origin.txt metadata into beets MusicBrainz queries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/x1ppy/beets-originquery",
+    python_requires='>=3.6',
+    install_requires=[
+        "beets>=1.5.0",
+    ],
+)
